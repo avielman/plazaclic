47deeac8731b4a26e63d915e50f1880dde0cbd2e
avielman <avielman@gmail.com>
2025-10-06 22:56:09 -0600

    10002: Cambios menores, pruebas funcionales.

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.plazaclic.com/api',
+  appName: 'plazaclic',
+  enableDebug: false,
+  host: 'http://plazaclic.com:4200'
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,9 @@
+import { hostname } from "os";
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  appName: 'plazaclic',
+  enableDebug: true,
+  host: 'http://' + hostname() + ':4200'
+};
